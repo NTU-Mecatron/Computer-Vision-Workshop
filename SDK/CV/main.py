e322a385b46ecf871ad9bf43e294ae3724905018
Scottie C <48780312+scott-cjx@users.noreply.github.com>
2024-09-03 11:18:29 +0800

    working cv open webcam

--- SDK/CV/main.py
+++ SDK/CV/main.py
@@ -1,14 +1,12 @@
 import cv2
-from matplotlib import pyplot as plt
-import yaml
 
 
 class ObjectTracker():
-    camera_stream_path = 0
+    camera_stream_path = 1
     cap = None
     cap_frame = None
 
-    def __init__(self, camera_stream_path = 0):
+    def __init__(self, camera_stream_path = 1):
         self.camera_stream_path = camera_stream_path
     
     def openStream(self):
@@ -19,20 +17,27 @@ class ObjectTracker():
 
     def readStream(self):
         ret, self.cap_frame = self.cap.read()
+        return ret
     
     def showStream(self):
-        cv2.imshow(self.__name__, self.cap)
+        cv2.imshow("frame", self.cap_frame)
+    
+    def closeStream(self):
+        cv2.destroyAllWindows()
+        self.cap.release()
     
     def run(self):
         self.openStream()
         while 1:
             self.readStream()
             self.showStream()
-            if(cv2.waitKey(0) == 'q'):
-                cv2.destroyAllWindows()
-
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
 
 def main():
     objectTracker = ObjectTracker()
     objectTracker.run()
 
+
+if __name__ == "__main__":
+    main()
